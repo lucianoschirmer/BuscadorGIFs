5db34884e4dd59199aa289fca6dcbccdc362c8e7
lucianoschirmer <luciano.schirmer@wtti.com.br>
2020-10-22 13:01:10 -0300

    finalizado

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)))),
+  ));
+}
